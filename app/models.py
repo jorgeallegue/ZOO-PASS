00cca24c55ff6b12f36cc6cc7f3783fabdc54ab1
Jorge Allegue <jorgeallegue@gmail.com>
2024-07-09 21:23:20 +0200

    entradas por tipos contabilizadas

--- app/models.py
+++ app/models.py
@@ -6,10 +6,12 @@ class TipoEntrada(Enum):
     ADULTO = auto()
     JUBILADO = auto()
 
-
 class Entrada:
+
     def __init__(self, edad: int):
-        if edad <= 2:
+        if edad <0:
+            self.__validate_edad(edad)
+        elif edad <= 2:
             self.tipo = TipoEntrada.BEBE
             self.precio = 0
         elif edad <= 13:
@@ -21,3 +23,33 @@ class Entrada:
         else:
             self.tipo = TipoEntrada.JUBILADO
             self.precio = 18
+    def __validate_edad(self, edad):
+        if edad < 0:
+            raise ValueError("La edad no debe ser negativa")
+
+class Grupo_Entrada:
+    def __init__(self):
+        self.total = 0
+        self.num_entradas = 0
+        self.tipos_entrada = {
+            TipoEntrada.BEBE: 0,
+            TipoEntrada.NIÑO: 0,
+            TipoEntrada.ADULTO: 0,
+            TipoEntrada.JUBILADO: 0
+        }
+
+    def add_entrada(self, edad):
+        """
+        en funcion de la edad, crear e incrementar el contador de 
+        entradas.
+        Con el precio de la entrada nueva incrementar el total
+        """
+        nueva_entrada = Entrada(edad)
+        self.num_entradas += 1
+        self.total += nueva_entrada.precio
+
+        self.tipos_entrada[nueva_entrada.tipo] += 1    
+    
+    def cantidad_entrada_por_tipo(self, tipo: TipoEntrada):
+        return self.tipos_entrada[tipo]
+
--- tests/test_models.py
+++ tests/test_models.py
@@ -1,4 +1,5 @@
-from app.models import Entrada, TipoEntrada
+from app.models import Entrada, TipoEntrada, Grupo_Entrada
+import pytest
 
 def test_crear_entrada():
     entrada = Entrada(12)
@@ -17,4 +18,52 @@ def test_crear_entrada():
     assert entrada.tipo == TipoEntrada.BEBE
     assert entrada.precio == 0
 
+def test_crear_entrada_edad_negativa_error():
+    with pytest.raises(ValueError):
+        Entrada(-2)
+    """
+    esto sería equivalente a lo de arriba
+    try:
+        entrada = Entrada(-2)
+        assert False, "No ha saltado ValueError"
+    except ValueError:
+        assert True 
+    """
+
+def test_crear_grupo_entradas():
+    grupo = Grupo_Entrada()
+    assert grupo.total == 0
+    assert  grupo.num_entradas == 0
+
+def test_añadir_entradas_a_grupos():
+
+    grupo = Grupo_Entrada()
+    grupo.add_entrada(35)
+    assert grupo.num_entradas == 1
+    assert grupo.total == 23
     
+    grupo.add_entrada(12)
+    assert grupo.num_entradas == 2
+    assert grupo.total == 37
+
+    grupo.add_entrada(70)
+    assert grupo.num_entradas == 3
+    assert grupo.total == 55
+
+    grupo.add_entrada(2)
+    assert grupo.num_entradas == 4
+    assert grupo.total == 55
+
+def test_cantidad_entrada_por_tipo():
+    grupo = Grupo_Entrada()
+    grupo.add_entrada(10)
+    grupo.add_entrada(70)
+    grupo.add_entrada(35)
+    grupo.add_entrada(2)
+
+    assert grupo.cantidad_entrada_por_tipo(TipoEntrada.NIÑO) == 1
+    assert grupo.cantidad_entrada_por_tipo(TipoEntrada.JUBILADO) == 1
+    assert grupo.cantidad_entrada_por_tipo(TipoEntrada.ADULTO) == 1
+    assert grupo.cantidad_entrada_por_tipo(TipoEntrada.BEBE) == 1
+
+
